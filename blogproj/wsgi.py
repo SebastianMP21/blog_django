6dd7e1b2ae08e4985e2e1989210c28ab95797f8b
SebastianMP21 <sebas_slark123@outlook.com>
2023-11-29 21:52:28 -0500

    posible hosting :/

--- blogproj/wsgi.py
+++ blogproj/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+path = os.path.expanduser('~/SebastianMP21')
+if path not in sys.path:
+    sys.path.insert(0,path)
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'blogproj.settings'
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogproj.settings')
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
